bb37ed7b81aeec7eab38c8879f1379c5bc1f6181
Huy Tran <Nicudes@gmail.com>
2021-02-04 22:40:52 +0100

    Fixed mobile media querys

--- javascript/script.js
+++ javascript/script.js
@@ -3,36 +3,13 @@ $(document).ready(function(){
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        // centerMode: true,
-
-      }
-
-    }, {
+    responsive: [
+        {
       breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: true,
-        infinite: true,
-
-      }
+      settings: "unslick"
     },  {
       breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      }
+      settings: "unslick"
     }]
   });
 });
-
-
