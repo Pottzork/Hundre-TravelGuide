948e5a9c71e83158e08e29701581c4494c86cb2a
Huy Tran <Nicudes@gmail.com>
2021-02-15 15:55:20 +0100

    new theme for index

--- javascript/script.js
+++ javascript/script.js
@@ -2,17 +2,41 @@ $(document).ready(function(){
   $('.carousel').slick({
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     responsive: [
-        {
-      breakpoint: 800,
-      settings: "unslick"
-    },  {
-      breakpoint: 480,
-      settings: "unslick"
-    }]
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
-     $(window).on('resize', function() {
-        $('.carousel').slick('resize');
-    });
+});
+
+$('.news-carousel').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 700,
+  arrows: false,
 });
