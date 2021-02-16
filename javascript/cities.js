32d6bdaedeb4edafc5cc7d1f75715cecbec78fa8
André Pontes <33063611+Pottzork@users.noreply.github.com>
2021-02-16 22:20:03 +0100

    added jsonfile which we can read from, also added like function

---
+++ javascript/cities.js
@@ -0,0 +1,54 @@
+fetch("../json/travel-json.json")
+    .then(response => response.json())
+    .then(data => {
+        var bergenLikes = data.Bergen.likes
+        var londonLikes = data.London.likes
+        var pragueLikes = data.Prague.likes
+        var baliLikes = data.Bali.likes
+        var tokyoLikes = data.Tokyo.likes
+        document.getElementById("bergenLikes").innerHTML = " " + bergenLikes;
+        document.getElementById("londonLikes").innerHTML = " " + londonLikes;
+        document.getElementById("pragueLikes").innerHTML = " " + pragueLikes;
+        document.getElementById("baliLikes").innerHTML = " " + baliLikes;
+        document.getElementById("tokyoLikes").innerHTML = " " + tokyoLikes;
+        console.log(document.getElementById('bergenLikes').value);
+    })
+
+
+function incrementValue(city) {
+    var cityHeart = document.getElementById(city + "Heart")
+    var value = parseInt(document.getElementById(city + "Likes").innerHTML);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById(city + "Likes").innerHTML = value;
+
+    if (cityHeart.style.color != "red") {
+        cityHeart.style.color = "red";
+    } if (cityHeart.style.color == "red") {
+        cityHeart.style.color = "none";
+    }
+
+
+
+}
+
+
+// function incrementValue() {
+//     var bergenHeart = document.getElementById('bergenHeart')
+//     var value = parseInt(document.getElementById('bergenLikes').innerHTML);
+//     value = isNaN(value) ? 0 : value;
+//     value++;
+//     document.getElementById('bergenLikes').innerHTML = value;
+
+
+//     if (bergenHeart.style.color != "red") {
+//         bergenHeart.style.color = "red";
+//     } if (bergenHeart.style.color == "red") {
+//         ergenHeart.style.color = "none";
+//     }
+
+
+// data.destinations['Europe'][0].country   // Sentra
+// data.destinations['Europe'][0].city   // Maxima
+// data.destinations['Europe'][0].likes   // 2
+
